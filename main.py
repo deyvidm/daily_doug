32afd0b52220b7babd509135a888913f809dd2b1
deyvid <dave.mitkov@gmail.com>
2019-03-22 11:28:08 -0400

    reformatted aboslute path logic

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ from bs4 import BeautifulSoup
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-CONFIG_PATH = './config.yml'
-LOG_PATH =    './runlog'
+CONFIG_PATH = os.path.join(__location__, './config.yml')
+LOG_PATH =    os.path.join(__location__, './runlog')
 
 logging.basicConfig(filename=LOG_PATH, format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 logging.info("\n{}\nRun started\n".format("="*80))
@@ -129,7 +129,7 @@ def scrape_checkin(checkin_container) -> dict:
     return checkin
 
 def import_config(config_path: str) -> object:
-    f = open(os.path.join(__location__, config_path), 'r')
+    f = open(config_path, 'r')
     config = yaml.safe_load(f)
     return config
 
@@ -146,7 +146,7 @@ def gather_checkins(http_response_obj: requests.Response) :
 
 def write_latest_checkin_id(latest_checkin_id):
     config['last_checkin_id'] = latest_checkin_id
-    with open(os.path.join(__location__, CONFIG_PATH), 'w') as outfile:
+    with open(CONFIG_PATH, 'w') as outfile:
         yaml.dump(config, outfile, default_flow_style=False)
 
 config = import_config(CONFIG_PATH)
